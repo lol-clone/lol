d3058b1aff507a42418b67e7f664724e16896cd0
muhwa91 <muhwa91@gmail.com>
2023-11-21 00:10:53 +0900

    1121 start

---
+++ lol_vue/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="nav_container">
+    <div class="nav_img">
+      <img src="./assets/img/nav_img.png" alt="">
+    </div>
+    <div class="nav_a">
+      <a href="#">게임정보</a>
+      <a href="#">새소식</a>
+      <a href="#">다운로드</a>
+      <a href="#">E스포츠</a>
+      <a href="#">이벤트</a>
+      <a href="#">알아보기</a>
+      <a href="#">유니버스</a>
+      <a href="#">라이엇 스토어</a>
+      <a href="#">고객지원</a>
+    </div>
+  </div>
+  <div class="nav_button">
+    <button class="login">로그인</button>
+    <button type="button" class="join">회원가입</button>
+  </div>
+  
+  
+  <main></main>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</template>
+
+<script>
+// import data from './assets/js/common.js';
+
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+@import url(./assets/css/common.css);
+
+</style>
